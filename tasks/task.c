4ea7dae6bee8f980385b85234053597b6905ba5f
Eben <146240499+ne-njaravani@users.noreply.github.com>
2024-02-05 11:42:52 +0000

    Successfully implemeted command line args.

--- tasks/task.c
+++ tasks/task.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-    int num1, num2;
+int main(int argc, char *argv[])
+
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <filename> <number of students>\n", argv[0]);
+        return 0;
+    }
+
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
 
     // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+    // printf("Enter the first number: ");
+    // scanf("%d", &num1);
 
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
+    // printf("Enter the second number: ");
+    // scanf("%d", &num2);
 
     // Calculate and print the sum
     int sum = num1 + num2;
